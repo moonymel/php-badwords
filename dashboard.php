949abf330b60b0914e2254dbc9af481e8a40e6f1
moonymel <musumeci.me@gmail.com>
2024-01-23 18:04:45 +0100

    [UPDATE] my_text completed in dashboard.php

--- dashboard.php
+++ dashboard.php
@@ -1,11 +1,27 @@
+<?php 
+    $bad_word = $_GET['bad_word'];
+    $my_text = $_GET['my_text'];
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet" href="./css/style.css">
 </head>
 <body>
-    
+    <div class="container">
+        <div class="row my-4">
+            <div class="col-6">
+                <h3>Il tuo testo:</h3>
+                <?php echo $my_text ?>
+                <h4>Lunghezza del testo:</h4>
+                <?php echo strlen($my_text); ?> caratteri.
+            </div>
+        </div>
+    </div>
 </body>
 </html>
